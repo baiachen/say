d7d897fff3c6e457d911617b3aae8b840cff20c3
baia <baia@baias-MacBook-Pro.local>
2018-08-04 16:10:47 +0800

    first commit

---
+++ src/Say.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baia
+ * Date: 2018/8/4
+ * Time: 下午3:46
+ */
+
+namespace Baiachen\Say;
+
+
+class Say
+{
+    public static function hi(string $string)
+    {
+        return sprintf("Hello World. %s", $string);
+    }
+
+}
---
+++ tests/SayTest.php
@@ -0,0 +1,32 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: baia
+ * Date: 2018/8/4
+ * Time: 下午3:52
+ */
+
+namespace Baiachen\Say\Test;
+
+
+use Baiachen\Say\Say;
+use PHPUnit\Framework\TestCase;
+
+class SayTest extends TestCase
+{
+    /** @test */
+    public function 想说什么说什么()
+    {
+        //arrange
+        $something = Say::hi('说些什么吧');
+
+
+        //act
+        $act = 'Hello World. 说些什么吧';
+
+        //assert
+        $this->assertSame($act, $something);
+
+    }
+
+}
